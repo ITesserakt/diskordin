50a3e8df0d4c9fb9343b04522b90f3f903816dd5
ITesserakt <potryas66@mail.ru>
2019-10-30 21:24:13 +0300

    Added missed event. Removed unused parameter in Opcode

--- src/main/kotlin/ru/tesserakt/diskordin/gateway/json/Opcode.kt
+++ src/main/kotlin/ru/tesserakt/diskordin/gateway/json/Opcode.kt
@@ -1,25 +1,18 @@
 package ru.tesserakt.diskordin.gateway.json
 
-import ru.tesserakt.diskordin.gateway.json.commands.Identify
-import ru.tesserakt.diskordin.gateway.json.commands.InvalidSession
-import ru.tesserakt.diskordin.gateway.json.commands.Resume
-import ru.tesserakt.diskordin.gateway.json.events.HeartbeatACK
-import ru.tesserakt.diskordin.gateway.json.events.Hello
-import kotlin.reflect.KClass
-
-enum class Opcode(val type: KClass<out IPayload>) {
-    DISPATCH(IRawEvent::class),
-    HEARTBEAT(Heartbeat::class),
-    IDENTIFY(Identify::class),
-    STATUS_UPDATE(IPayload::class), //TODO: Stub
-    VOICE_STATUS_UPDATE(IPayload::class), //TODO: Stub
-    VOICE_SERVER_PING(IPayload::class), //TODO: Stub
-    RESUME(Resume::class),
-    RECONNECT(Nothing::class),
-    REQUEST_GUILD_MEMBERS(IPayload::class), //TODO: Stub
-    INVALID_SESSION(InvalidSession::class),
-    HELLO(Hello::class),
-    HEARTBEAT_ACK(HeartbeatACK::class);
+enum class Opcode {
+    DISPATCH,
+    HEARTBEAT,
+    IDENTIFY,
+    STATUS_UPDATE,
+    VOICE_STATUS_UPDATE,
+    VOICE_SERVER_PING,
+    RESUME,
+    RECONNECT,
+    REQUEST_GUILD_MEMBERS,
+    INVALID_SESSION,
+    HELLO,
+    HEARTBEAT_ACK;
 }
 
 fun Int.asOpcode() = Opcode.values().first { it.ordinal == this }
