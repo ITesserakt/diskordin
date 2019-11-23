c5b77b25b77edc23c8578e36a947bf583767de8c
ITesserakt <potryas66@mail.ru>
2019-11-23 21:08:14 +0300

    Improved versioning in build.gradle

--- src/main/kotlin/org/tesserakt/diskordin/impl/core/client/ServicesSetup.kt
+++ src/main/kotlin/org/tesserakt/diskordin/impl/core/client/ServicesSetup.kt
@@ -27,7 +27,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 import java.io.File
 import java.util.concurrent.TimeUnit
 
-private val libraryVersion get() = GlobalContext.get().koin.getProperty("lib-ver", "Undefined")
+private val libraryVersion get() = GlobalContext.get().koin.getProperty("diskordin_version", "Undefined")
 
 internal fun setupRetrofit(discordApiUrl: String, httpClient: OkHttpClient) = Retrofit.Builder()
     .client(httpClient)
