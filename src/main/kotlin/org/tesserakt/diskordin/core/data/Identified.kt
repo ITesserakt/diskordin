b8d08c6867a9e338c4b5883823fba2986bff364c
Владимир <37984459+itesserakt@users.noreply.github.com>
2019-12-17 18:25:12 +0300

    More flexible Identified

--- src/main/kotlin/org/tesserakt/diskordin/core/data/Identified.kt
+++ src/main/kotlin/org/tesserakt/diskordin/core/data/Identified.kt
@@ -1,7 +1,10 @@
 package org.tesserakt.diskordin.core.data
 
 import arrow.Kind
+import arrow.core.ForId
 import arrow.mtl.Kleisli
+import arrow.typeclasses.Functor
+import arrow.typeclasses.Monad
 import org.tesserakt.diskordin.core.entity.IEntity
 
 data class IdentifiedF<F, E : IEntity>(
@@ -9,7 +12,16 @@ data class IdentifiedF<F, E : IEntity>(
     private val render: Kleisli<F, Snowflake, E>
 ) {
     operator fun invoke() = render.run(id)
+
+    fun <B : IEntity> map(FN: Functor<F>, f: (E) -> B) =
+        id identify render.map(FN, f).run
+
+    fun <B : IEntity> flatMap(M: Monad<F>, f: (E) -> IdentifiedF<F, B>) = id identify render.flatMap(M) {
+        f(it).render
+    }.run
 }
+
+typealias Identified<E> = IdentifiedF<ForId, E>
 
 infix fun <F, E : IEntity> Snowflake.identify(render: (Snowflake) -> Kind<F, E>) =
     IdentifiedF(this, Kleisli(render))
--- src/main/kotlin/org/tesserakt/diskordin/core/data/event/message/MessageCreateEvent.kt
+++ src/main/kotlin/org/tesserakt/diskordin/core/data/event/message/MessageCreateEvent.kt
@@ -1,19 +1,27 @@
 package org.tesserakt.diskordin.core.data.event.message
 
 import arrow.core.ForId
+import arrow.core.Id
 import arrow.core.extensions.id.applicative.just
 import arrow.core.extensions.id.comonad.extract
+import arrow.core.extensions.id.monad.monad
 import arrow.fx.ForIO
+import arrow.fx.IO
+import arrow.fx.extensions.io.monad.monad
 import org.tesserakt.diskordin.core.data.event.channel.IChannelEvent
 import org.tesserakt.diskordin.core.data.identify
 import org.tesserakt.diskordin.core.data.json.response.MessageResponse
 import org.tesserakt.diskordin.core.data.json.response.unwrap
+import org.tesserakt.diskordin.core.entity.IGuildChannel
 import org.tesserakt.diskordin.rest.storage.GlobalEntityCache
 
 class MessageCreateEvent(raw: MessageResponse) : IMessageEvent<ForId>, IChannelEvent<ForIO> {
     override val message = raw.id identify { raw.unwrap().just() }
     override val channel = message().extract().channel
-    val author = message().extract().author
+    val author = message.flatMap(Id.monad()) { it.author }
+    val guild = channel.takeIf { it is IGuildChannel }?.flatMap(IO.monad()) {
+        (it as IGuildChannel).guild
+    }
 
     init {
         GlobalEntityCache[message.id] = message().extract()
