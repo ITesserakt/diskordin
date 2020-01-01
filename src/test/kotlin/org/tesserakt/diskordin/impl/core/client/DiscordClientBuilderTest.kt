f33dc252f02e411476af3971824c99c8071816f1
Владимир <37984459+itesserakt@users.noreply.github.com>
2020-01-01 16:47:41 +0300

    Removed implicit token in DiscordClientBuilderTest

--- src/test/kotlin/org/tesserakt/diskordin/impl/core/client/DiscordClientBuilderTest.kt
+++ src/test/kotlin/org/tesserakt/diskordin/impl/core/client/DiscordClientBuilderTest.kt
@@ -6,26 +6,42 @@ import org.amshove.kluent.`should throw`
 import org.amshove.kluent.`with message`
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable
 
-@EnabledIfEnvironmentVariable(named = "token", matches = ".*")
+private const val token = "NTQ3ND5MTA3NTg1MDA3NjM.123456.UiyyQPPomIp_uzpGgEwhHY" //doesn't work with real bot
+
 internal class DiscordClientBuilderTest {
     @Test
     fun `create instance of client once`() {
-        { DiscordClientBuilder {} } `should not throw` IllegalStateException::class `with message` "Discord client already created"
+        {
+            DiscordClientBuilder {
+                +token(token)
+            }
+        } `should not throw` IllegalStateException::class `with message` "Discord client already created"
     }
 
     @Test
     fun `create instance of client twice should produce error`() {
-        DiscordClientBuilder { };
-        { DiscordClientBuilder { } } `should throw` IllegalStateException::class `with message` "Discord client already created"
+        DiscordClientBuilder {
+            +token(token)
+        };
+        {
+            DiscordClientBuilder {
+                +token(token)
+            }
+        } `should throw` IllegalStateException::class `with message` "Discord client already created"
     }
 
     @Test
     fun `create client, logout and create again`() {
-        val cl = DiscordClientBuilder {}
+        val cl = DiscordClientBuilder {
+            +token(token)
+        }
         cl.logout();
-        { DiscordClientBuilder {} } `should not throw` IllegalStateException::class `with message` "Discord client already created"
+        {
+            DiscordClientBuilder {
+                +token(token)
+            }
+        } `should not throw` IllegalStateException::class `with message` "Discord client already created"
     }
 
     @AfterEach
