544d56c62ef73bee80a28b671816997dbc9e4513
Владимир <37984459+itesserakt@users.noreply.github.com>
2020-03-03 22:03:40 +0300

    `All`, `none` methods for ValuedEnum and small improvements
    (cherry picked from commit e6c0e308eca08933e9e62caf0d24f4e6cf90759a)

--- src/main/kotlin/org/tesserakt/diskordin/util/enums/IValued.kt
+++ src/main/kotlin/org/tesserakt/diskordin/util/enums/IValued.kt
@@ -3,46 +3,26 @@
 package org.tesserakt.diskordin.util.enums
 
 import org.tesserakt.diskordin.util.typeclass.Integral
-import java.util.*
 
-interface IValued<E : Enum<E>, N> : Integral<N> {
+interface IValued<E : Enum<E>, N : Any> : Integral<N> {
     val code: N
 }
 
-infix fun <E, N> IValued<E, N>.and(other: IValued<E, N>)
+infix fun <E, N : Any> IValued<E, N>.and(other: IValued<E, N>)
         where E : Enum<E>, E : IValued<E, N> =
     ValuedEnum<E, N>(code and other.code, this)
 
-infix fun <E, N> IValued<E, N>.or(other: IValued<E, N>)
+infix fun <E, N : Any> IValued<E, N>.or(other: IValued<E, N>)
         where E : Enum<E>, E : IValued<E, N> =
     ValuedEnum<E, N>(code or other.code, this)
 
-infix fun <E, N> IValued<E, N>.xor(other: IValued<E, N>)
+infix fun <E, N : Any> IValued<E, N>.xor(other: IValued<E, N>)
         where E : Enum<E>, E : IValued<E, N> =
     ValuedEnum<E, N>(code xor other.code, this)
 
-operator fun <E, N> E.not()
+operator fun <E, N : Any> E.not()
         where E : Enum<E>, E : IValued<E, N> = ValuedEnum<E, N>(
     this@not.declaringClass.enumConstants
         .map(IValued<E, N>::code)
         .reduce { acc, i -> acc + i } - code, this
 )
-
-inline operator fun <reified E, N> ValuedEnum<E, N>.not()
-        where E : Enum<E>, E : IValued<E, N> = ValuedEnum<E, N>(
-    E::class.java.enumConstants
-        .map { it.code }
-        .reduce { acc, n -> acc + n } - this.code, integral
-)
-
-inline fun <reified E, N> IValued<E, N>.asSet(): EnumSet<E>
-        where E : Enum<E>, E : IValued<E, N> = EnumSet.allOf(E::class.java).apply {
-    removeIf {
-        this@asSet.code and it.code != it.code
-    }
-}
-
-fun <E, N> EnumSet<E>.enhance(integral: Integral<N>)
-        where E : Enum<E>, E : IValued<E, N> = with(integral) {
-    ValuedEnum<E, N>(map { it.code }.reduce { acc, n -> acc + n }, integral)
-}
--- src/main/kotlin/org/tesserakt/diskordin/util/enums/ValuedEnum.kt
+++ src/main/kotlin/org/tesserakt/diskordin/util/enums/ValuedEnum.kt
@@ -1,8 +1,60 @@
 package org.tesserakt.diskordin.util.enums
 
 import org.tesserakt.diskordin.util.typeclass.Integral
+import java.util.*
 
 class ValuedEnum<E, I>(override val code: I, val integral: Integral<I>) : IValued<E, I>, Integral<I> by integral
-        where E : Enum<E>, E : IValued<E, I> {
+        where E : Enum<E>, E : IValued<E, I>, I : Any {
+
     operator fun contains(other: IValued<E, I>) = this.code and other.code == other.code
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as ValuedEnum<*, *>
+
+        if (code != other.code) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return code.hashCode()
+    }
+
+    override fun toString(): String {
+        return "ValuedEnum(code=$code)"
+    }
+
+    companion object {
+        fun <E, N> none(integral: Integral<N>)
+                where E : Enum<E>, N : Any, E : IValued<E, N> = integral.run {
+            ValuedEnum<E, N>(0.fromInt(), integral)
+        }
+
+        inline fun <reified E, N> all(integral: Integral<N>)
+                where E : Enum<E>, N : Any, E : IValued<E, N> = integral.run {
+            ValuedEnum<E, N>(
+                E::class.java.enumConstants
+                    .map { it.code }
+                    .fold(0.fromInt()) { acc, n -> acc + n }, integral
+            )
+        }
+    }
+}
+
+inline operator fun <reified E, N : Any> ValuedEnum<E, N>.not() where E : Enum<E>, E : IValued<E, N> =
+    ValuedEnum<E, N>(ValuedEnum.all<E, N>(integral).code - code, integral)
+
+inline fun <reified E, N : Any> ValuedEnum<E, N>.asSet(): EnumSet<E>
+        where E : Enum<E>, E : IValued<E, N> = EnumSet.allOf(E::class.java).apply {
+    removeIf {
+        code and it.code != it.code
+    }
+}
+
+fun <E, N : Any> EnumSet<E>.enhance(integral: Integral<N>)
+        where E : Enum<E>, E : IValued<E, N> = with(integral) {
+    ValuedEnum<E, N>(map { it.code }.fold(0.fromInt()) { acc, n -> acc + n }, integral)
 }
