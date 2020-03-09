4c49f5bc80db197b59bcf3ca2f0a8ff5b71d7f9c
Владимир <37984459+itesserakt@users.noreply.github.com>
2020-03-09 13:22:24 +0300

    Added annotations for commands

---
+++ diskordin-commands/src/main/kotlin/org/tesserakt/diskordin/commands/Annotations.kt
@@ -0,0 +1,30 @@
+package org.tesserakt.diskordin.commands
+
+/**
+ * This annotation used to show that this function is a command.
+ *
+ * If [name] is not specified then name of function used
+ */
+@MustBeDocumented
+@Target(AnnotationTarget.FUNCTION)
+annotation class Command(val name: String = "")
+
+/**
+ * Adds a description to a command
+ */
+@Target(AnnotationTarget.FUNCTION)
+annotation class Description(val description: String)
+
+/**
+ * Adds a set of aliases to a command
+ */
+@Target(AnnotationTarget.FUNCTION)
+annotation class Aliases(vararg val aliases: String)
+
+/**
+ * Hide a command from commands list, but it can be run from Discord by name
+ *
+ * May be applied to function to hide only one command or class to hide all commands in this class
+ */
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.CLASS)
+annotation class Hide
