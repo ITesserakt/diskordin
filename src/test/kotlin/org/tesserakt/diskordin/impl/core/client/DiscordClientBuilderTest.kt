573d2a4ed9dc164e1c2e5904c30a523b1780c845
Владимир <37984459+itesserakt@users.noreply.github.com>
2020-03-15 13:37:04 +0300

    Rewritten test for DiscordClientBuilder to kotest version

--- src/test/kotlin/org/tesserakt/diskordin/impl/core/client/DiscordClientBuilderTest.kt
+++ src/test/kotlin/org/tesserakt/diskordin/impl/core/client/DiscordClientBuilderTest.kt
@@ -1,45 +1,53 @@
 package org.tesserakt.diskordin.impl.core.client
 
+import arrow.fx.ForIO
 import arrow.fx.fix
-import org.amshove.kluent.`should not throw`
-import org.amshove.kluent.`should throw`
-import org.amshove.kluent.`with message`
-import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.Timeout
-import java.util.concurrent.TimeUnit
+import io.kotest.assertions.throwables.shouldNotThrow
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.core.test.TestCase
+import io.kotest.core.test.TestCaseConfig
+import io.kotest.core.test.TestResult
+import org.amshove.kluent.shouldBe
+import kotlin.time.ExperimentalTime
+import kotlin.time.seconds
 
+@ExperimentalTime
 @DiscordClientBuilder.InternalTestAPI
-internal class DiscordClientBuilderTest {
-    @Test
-    @Timeout(1, unit = TimeUnit.SECONDS)
-    fun `create instance of client once`() {
-        {
-            DiscordClientBuilder.default { +disableTokenVerification() }
-        } `should not throw` IllegalStateException::class `with message` "Discord client already created"
+class DiscordClientBuilderTest : StringSpec() {
+    private fun DiscordClientBuilder.Companion.test(f: DiscordClientBuilder<ForIO>.() -> Unit = {}) =
+        default {
+            f()
+            +disableTokenVerification()
+        }
+
+    init {
+        //to check whether http client started
+        defaultTestConfig = TestCaseConfig(timeout = 1.seconds)
+
+        "Single creation of DiscordClientBuilder should not produce error" {
+            shouldNotThrow<IllegalStateException> {
+                DiscordClientBuilder.test()
+            }
+        }
+
+        "Twice creation without close should produce error" {
+            DiscordClientBuilder.test()
+            shouldThrow<IllegalStateException> {
+                DiscordClientBuilder.test()
+            }.message shouldBe "Discord client already created"
+        }
+
+        "Twice creation with close should not produce error" {
+            val client = DiscordClientBuilder.test()
+            client.logout()
+            shouldNotThrow<IllegalStateException> {
+                DiscordClientBuilder.test()
+            }
+        }
     }
 
-    @Test
-    @Timeout(1, unit = TimeUnit.SECONDS)
-    fun `create instance of client twice should produce error`() {
-        DiscordClientBuilder.default { +disableTokenVerification() };
-        {
-            DiscordClientBuilder.default { +disableTokenVerification() }
-        } `should throw` IllegalStateException::class `with message` "Discord client already created"
-    }
-
-    @Test
-    @Timeout(1, unit = TimeUnit.SECONDS)
-    fun `create client, logout and create again`() {
-        val cl = DiscordClientBuilder.default { +disableTokenVerification() }
-        cl.logout();
-        {
-            DiscordClientBuilder.default { +disableTokenVerification() }
-        } `should not throw` IllegalStateException::class `with message` "Discord client already created"
-    }
-
-    @AfterEach
-    fun tearDown() {
+    override fun afterTest(testCase: TestCase, result: TestResult) {
         DiscordClient.client.set(null).fix().unsafeRunSync()
     }
 }
