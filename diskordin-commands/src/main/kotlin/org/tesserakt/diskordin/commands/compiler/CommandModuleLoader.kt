6ae42b1f05208a8f3a2e0ca3a12559d13ed89f40
Владимир <37984459+itesserakt@users.noreply.github.com>
2020-03-23 01:01:05 +0300

    Properly closing resources and scanning packages

--- diskordin-commands/src/main/kotlin/org/tesserakt/diskordin/commands/compiler/CommandModuleLoader.kt
+++ diskordin-commands/src/main/kotlin/org/tesserakt/diskordin/commands/compiler/CommandModuleLoader.kt
@@ -1,15 +1,16 @@
 package org.tesserakt.diskordin.commands.compiler
 
-import arrow.core.NonEmptyList
-import arrow.core.Validated
-import arrow.core.ValidatedPartialOf
+import arrow.core.*
 import arrow.core.extensions.list.traverse.sequence
 import arrow.core.extensions.nonemptylist.semigroup.semigroup
 import arrow.core.extensions.validated.applicative.applicative
 import arrow.core.extensions.validated.traverse.traverse
-import arrow.core.fix
 import arrow.fx.IO
+import arrow.fx.Resource
 import arrow.fx.extensions.io.applicative.just
+import arrow.fx.extensions.io.bracket.bracket
+import arrow.fx.fix
+import arrow.fx.typeclasses.ExitCase
 import io.github.classgraph.ClassGraph
 import io.github.classgraph.ClassInfo
 import io.github.classgraph.ScanResult
@@ -18,6 +19,7 @@ import org.tesserakt.diskordin.commands.CommandBuilder
 import org.tesserakt.diskordin.commands.CommandObject
 import org.tesserakt.diskordin.commands.CommandRegistry
 import org.tesserakt.diskordin.commands.ValidationError
+import java.util.concurrent.Executors
 
 @Suppress("unused")
 class CommandModuleLoader(
@@ -36,11 +38,20 @@ class CommandModuleLoader(
             if (specifiedPackage != null) it.whitelistPackages(specifiedPackage)
         }
 
-    fun load() = IO {
-        logger.debug("Inspecting packages for command modules")
-        if (parallelism != null) graph.scan(parallelism)
-        else graph.scan()
-    }.flatMap(::loadToRegistry)
+    fun load() = Resource(
+        { Executors.newFixedThreadPool(parallelism ?: 1).just() },
+        { threads, _: ExitCase<Throwable> -> threads.shutdown().just() },
+        IO.bracket()
+    ).flatMap { service ->
+        Resource({
+            IO.async<ScanResult> { sink ->
+                logger.debug("Inspecting ${specifiedPackage ?: ""} package for command modules")
+                graph.scanAsync(service, parallelism ?: 1, { sink(it.right()) }, { sink(it.left()) })
+            }
+        }, { scan, _: ExitCase<Throwable> -> scan.close().just() },
+            IO.bracket()
+        )
+    }.use(::loadToRegistry).fix()
 
     private fun ScanResult.commandModules() =
         allClasses.filter { it.extendsSuperclass("org.tesserakt.diskordin.commands.CommandModule") }
