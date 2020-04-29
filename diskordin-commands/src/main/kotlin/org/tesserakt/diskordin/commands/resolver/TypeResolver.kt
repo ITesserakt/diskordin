5a75b46dc4c9f0c587f6008d9a5e18db8fe82036
Владимир <37984459+itesserakt@users.noreply.github.com>
2020-04-29 11:01:53 +0300

    Replaced OptionT with more simple alternative

--- diskordin-commands/src/main/kotlin/org/tesserakt/diskordin/commands/resolver/TypeResolver.kt
+++ diskordin-commands/src/main/kotlin/org/tesserakt/diskordin/commands/resolver/TypeResolver.kt
@@ -1,7 +1,8 @@
 package org.tesserakt.diskordin.commands.resolver
 
+import arrow.Kind
+import arrow.core.rightIfNotNull
 import arrow.mtl.EitherT
-import arrow.mtl.OptionT
 import arrow.typeclasses.Functor
 import org.tesserakt.diskordin.commands.CommandContext
 import org.tesserakt.diskordin.core.entity.IMentioned
@@ -14,15 +15,15 @@ interface TypeResolver<T : Any, F, in C : CommandContext<F>> {
 interface MentionableTypeResolver<T : IMentioned, F, in C : CommandContext<F>> : TypeResolver<T, F, C>, Functor<F> {
     data class NotFoundError(val input: String) : ParseError("Could not find any object by '$input'")
 
-    fun parseByMention(context: C, input: String): OptionT<F, T>
-    fun parseById(context: C, input: String): OptionT<F, T>
-    fun rawParse(context: C, input: String): OptionT<F, T>
+    fun parseByMention(context: C, input: String): Kind<F, T?>
+    fun parseById(context: C, input: String): Kind<F, T?>
+    fun rawParse(context: C, input: String): Kind<F, T?>
     val static: StaticMention<T, *>
 
     override fun parse(context: C, input: String) = when {
         static.mention.matches(input) -> parseByMention(context, input)
         Regex("""\d{18,}""").matches(input) -> parseById(context, input)
         else -> rawParse(context, input)
-    }.toRight(this) { NotFoundError(input) }
+    }.map { it.rightIfNotNull { NotFoundError(input) } }.let { EitherT(it) }
 }
 
