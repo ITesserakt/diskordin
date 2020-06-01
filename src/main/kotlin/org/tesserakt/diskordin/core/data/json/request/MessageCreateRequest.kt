d159c93b54aa7d8257efb5e4e2f02d68b50042fb
Владимир <37984459+itesserakt@users.noreply.github.com>
2020-06-01 16:23:44 +0300

    Added mechanic to control mentions in messages

--- src/main/kotlin/org/tesserakt/diskordin/core/data/json/request/MessageCreateRequest.kt
+++ src/main/kotlin/org/tesserakt/diskordin/core/data/json/request/MessageCreateRequest.kt
@@ -1,8 +1,21 @@
 package org.tesserakt.diskordin.core.data.json.request
 
+import org.tesserakt.diskordin.core.data.Snowflake
+
 data class MessageCreateRequest(
     val content: String?,
     val nonce: Long? = null,
     val tts: Boolean? = null,
-    val embed: EmbedCreateRequest? = null
-) : JsonRequest()
+    val embed: EmbedCreateRequest? = null,
+    val allowedMentions: AllowedMentionsRequest? = null
+) : JsonRequest() {
+    data class AllowedMentionsRequest(
+        val parse: List<String>,
+        val roles: List<Snowflake>,
+        val users: List<Snowflake>
+    ) : JsonRequest() {
+        enum class AllowedMentionTypes(val value: String) {
+            Role("roles"), User("users"), Everyone("everyone")
+        }
+    }
+}
--- src/main/kotlin/org/tesserakt/diskordin/core/entity/builder/MessageCreateBuilder.kt
+++ src/main/kotlin/org/tesserakt/diskordin/core/entity/builder/MessageCreateBuilder.kt
@@ -5,6 +5,7 @@ package org.tesserakt.diskordin.core.entity.builder
 import arrow.core.Ior
 import org.tesserakt.diskordin.core.data.Snowflake
 import org.tesserakt.diskordin.core.data.json.request.MessageCreateRequest
+import org.tesserakt.diskordin.core.data.json.request.MessageCreateRequest.AllowedMentionsRequest.AllowedMentionTypes
 
 typealias Content = String
 typealias Embed = EmbedCreateBuilder
@@ -16,6 +17,43 @@ class MessageCreateBuilder(private val required: Ior<Content, Embed>) : BuilderB
     private var nonce: Snowflake? = null
     private var tts: Boolean? = null
     private var embed: EmbedCreateBuilder? = null
+    private var allowedMentions: MessageCreateRequest.AllowedMentionsRequest? = null
+
+    @RequestBuilder
+    class AllowedMentionsBuilder : BuilderBase<MessageCreateRequest.AllowedMentionsRequest>() {
+        private val parse = mutableSetOf<AllowedMentionTypes>()
+        private val roles = mutableSetOf<Snowflake>()
+        private val users = mutableSetOf<Snowflake>()
+
+        operator fun Array<out AllowedMentionTypes>.unaryPlus() {
+            parse += this.toSet()
+        }
+
+        operator fun Array<out Snowflake>.unaryPlus() {
+            require(this.size <= 100)
+
+            users += this.toSet()
+        }
+
+        operator fun List<Snowflake>.unaryPlus() {
+            require(this.size <= 100)
+
+            roles += this.toSet()
+        }
+
+        inline fun AllowedMentionsBuilder.allowOnly(vararg types: AllowedMentionTypes) = types
+        inline fun AllowedMentionsBuilder.allowUsers(vararg userIds: Snowflake) = userIds
+        inline fun AllowedMentionsBuilder.allowRoles(vararg roleIds: Snowflake) = roleIds.toList()
+        inline fun AllowedMentionsBuilder.suppressAll() {
+            +allowOnly()
+        }
+
+        override fun create() = MessageCreateRequest.AllowedMentionsRequest(
+            parse.map { it.value },
+            roles.toList(),
+            users.toList()
+        )
+    }
 
     override fun create(): MessageCreateRequest {
         required.fold({
@@ -31,7 +69,8 @@ class MessageCreateBuilder(private val required: Ior<Content, Embed>) : BuilderB
             content,
             nonce?.asLong(),
             tts,
-            embed?.create()
+            embed?.create(),
+            allowedMentions
         )
     }
 
@@ -43,6 +82,12 @@ class MessageCreateBuilder(private val required: Ior<Content, Embed>) : BuilderB
         tts = true
     }
 
+    operator fun MessageCreateRequest.AllowedMentionsRequest.unaryPlus() {
+        allowedMentions = this
+    }
+
     inline fun MessageCreateBuilder.nonce(id: Snowflake) = id
     inline fun MessageCreateBuilder.enableTTS() = Unit
+    inline fun MessageCreateBuilder.mentionRules(builder: AllowedMentionsBuilder.() -> Unit) =
+        AllowedMentionsBuilder().apply(builder).create()
 }
